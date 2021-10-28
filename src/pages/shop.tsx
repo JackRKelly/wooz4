45bce0b40fe7f6205916f6781e2ba4f8319a10d0
JackRKelly <kcjackkelly@gmail.com>
2021-10-28 15:37:26 -0500

    Implement product card for shop page

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -9,12 +9,20 @@ import {GetProducts, GetProductsVariables} from 'graph/@types/GetProducts';
 import {GET_PRODUCTS} from 'graph';
 import Link from 'next/link';
 import {LoadingSpinner} from 'components/LoadingSpinner';
+import {
+	ProductCardWrapper,
+	ProductCardLink,
+	ProductCard,
+	ProductFlex,
+	ProductCardImageWrapper,
+	ProductCardGrid,
+} from 'components/ProductCard';
 
 const Shop: NextPage = () => {
 	const {loading, data} = useQuery<GetProducts, GetProductsVariables>(
 		GET_PRODUCTS,
 		{
-			variables: {limit: 10},
+			variables: {limit: 20},
 		},
 	);
 	const {products} = data ?? {};
@@ -28,29 +36,40 @@ const Shop: NextPage = () => {
 			<LoadingSpinner isLoading={loading} />
 
 			<h1>Shop Wooz4</h1>
-			{products?.edges?.map(({node}) => {
-				const {
-					id,
-					title,
-					images: {edges: images},
-				} = node;
 
-				return (
-					<div key={id}>
-						<Link href={`/product/${id}`}>
-							<a>
-								<Image
-									alt={title}
-									src={images[0].node.transformedSrc as string}
-									width={100}
-									height={100}
-								/>
-								{title}
-							</a>
-						</Link>
-					</div>
-				);
-			})}
+			<ProductCardGrid>
+				{products?.edges?.map(({node}) => {
+					const {
+						id,
+						title,
+						priceRange: {minVariantPrice},
+						images,
+					} = node;
+
+					return (
+						<ProductCardWrapper key={id}>
+							<Link passHref href={`/product/${id}`}>
+								<ProductCardLink>
+									<ProductCard>
+										<ProductFlex>
+											<ProductCardImageWrapper>
+												<Image
+													alt="product"
+													src={images?.edges[0].node.transformedSrc as string}
+													layout="fill"
+												/>
+											</ProductCardImageWrapper>
+											<span>
+												{title} - ${minVariantPrice.amount}
+											</span>
+										</ProductFlex>
+									</ProductCard>
+								</ProductCardLink>
+							</Link>
+						</ProductCardWrapper>
+					);
+				})}
+			</ProductCardGrid>
 		</ContentColumn>
 	);
 };
