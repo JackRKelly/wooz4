15e490116de4ec4bfc9065a431f585fcf0dcb0e8
JackRKelly <kcjackkelly@gmail.com>
2021-10-28 14:05:50 -0500

    Add product card

--- src/components/FeaturedCollection.tsx
+++ src/components/FeaturedCollection.tsx
@@ -5,6 +5,9 @@ import {
 } from 'graph/@types/GetCollectionById';
 import {useQuery} from '@apollo/client';
 import React from 'react';
+import styled from 'styled-components';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const FeaturedCollection = () => {
 	const {data} = useQuery<GetCollectionById, GetCollectionByIdVariables>(
@@ -23,20 +26,70 @@ export const FeaturedCollection = () => {
 		<section>
 			<h3>{title}</h3>
 			<p>{description}</p>
-			{products?.edges.map(
-				({
-					node: {
-						id,
-						priceRange: {
-							minVariantPrice: {amount, currencyCode},
+			<ProductCardGrid>
+				{products?.edges.map(
+					({
+						node: {
+							id,
+							title,
+							images,
+							priceRange: {
+								minVariantPrice: {amount},
+							},
 						},
-					},
-				}) => (
-					<div key={id}>
-						product {id}, price {amount} {currencyCode}
-					</div>
-				),
-			)}
+					}) => (
+						<ProductCard key={id}>
+							<Link passHref href={`/product/${id}`}>
+								<ProductCardLink>
+									<ProductFlex>
+										<ProductCardImageWrapper>
+											<Image
+												alt="product"
+												src={images?.edges[0].node.transformedSrc as string}
+												layout="fill"
+											/>
+										</ProductCardImageWrapper>
+										<span>
+											{title} - {amount}
+										</span>
+									</ProductFlex>
+								</ProductCardLink>
+							</Link>
+						</ProductCard>
+					),
+				)}
+			</ProductCardGrid>
 		</section>
 	);
 };
+
+const ProductCard = styled.div`
+	background-color: white;
+	width: 15rem;
+	height: 18rem;
+	margin-bottom: 20px;
+`;
+
+const ProductCardImageWrapper = styled.div`
+	width: 15rem;
+	height: 15rem;
+	position: relative;
+`;
+
+const ProductCardGrid = styled.div`
+	display: grid;
+`;
+
+const ProductFlex = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	align-items: center;
+	justify-content: space-evenly;
+`;
+
+const ProductCardLink = styled.a`
+	color: black;
+	text-decoration: none;
+	cursor: pointer;
+`;
