7062293c6ef9e2d0c8c0a54c8964117a1de21507
JackRKelly <kcjackkelly@gmail.com>
2021-10-27 21:27:47 -0500

    implement select component

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -7,23 +7,55 @@ import {
 	GetProductById,
 	GetProductByIdVariables,
 } from 'graphql/@types/GetProductById';
+import Image from 'next/image';
+import {Select, Option} from 'components/Select';
 
 const Product = () => {
 	const router = useRouter();
 	const {id} = router.query;
 
-	const {data} = useQuery<GetProductById, GetProductByIdVariables>(
+	const {loading, data} = useQuery<GetProductById, GetProductByIdVariables>(
 		GET_PRODUCT_BY_ID,
 		{
 			variables: {id: id?.toString() ?? ''},
 		},
 	);
 
+	const {product} = data ?? {};
+	const {title, priceRange, options: productOptions, images} = product ?? {};
+
 	return (
 		<ContentColumn>
-			<h1>{data?.product?.title}</h1>
-			<p>Product ID: {id}</p>
-			<p>Price: ${data?.product?.priceRange.minVariantPrice.amount}</p>
+			{loading ? (
+				<p>loading (change)</p>
+			) : (
+				<div>
+					<h1>{title}</h1>
+					<p>Product ID: {id}</p>
+					<p>Price: ${priceRange?.minVariantPrice.amount}</p>
+					{productOptions?.map(option => {
+						const options: Option[] = option.values.map(value => ({
+							id: `${option.id}${value}`,
+							value,
+						}));
+						return (
+							<div key={option.id}>
+								<p>{option.name}</p>
+								<Select options={options} />
+							</div>
+						);
+					})}
+
+					{images?.edges[0]?.node?.transformedSrc && (
+						<Image
+							alt={title}
+							src={images?.edges[0]?.node?.transformedSrc as string}
+							width={300}
+							height={300}
+						/>
+					)}
+				</div>
+			)}
 		</ContentColumn>
 	);
 };
