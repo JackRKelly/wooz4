6b9332fb17f3cfead970164fba06a4258a8f7738
JackRKelly <kcjackkelly@gmail.com>
2021-10-27 22:35:37 -0500

    Consolidate and format keyframes

--- src/constants/keyframes.ts
+++ src/constants/keyframes.ts
@@ -1,40 +1,92 @@
-import {keyframes} from 'styled-components';
+import {css, keyframes} from 'styled-components';
 import {colors} from './colors';
 
 export const redBlueFade = keyframes`
-		0% {
-			stroke: ${colors.sakuraBlue};
-		}
-		48% {
-			stroke: ${colors.sakuraBlue};
-		}
-		52% {
-			stroke: ${colors.sakuraRed};
-		}
-		100% {
-			stroke: ${colors.sakuraRed};
-		}
+	0% {
+		stroke: ${colors.sakuraBlue};
+	}
+	48% {
+		stroke: ${colors.sakuraBlue};
+	}
+	52% {
+		stroke: ${colors.sakuraRed};
+	}
+	100% {
+		stroke: ${colors.sakuraRed};
+	}
 `;
 
 export const spinnerDash = keyframes`
-		0% {
-			stroke-dashoffset: 187;
-		}
-		50% {
-			stroke-dashoffset: 46.75;
-			transform: rotate(135deg);
-		}
-		100% {
-			stroke-dashoffset: 187;
-			transform: rotate(450deg);
-		}
+	0% {
+		stroke-dashoffset: 187;
+	}
+	50% {
+		stroke-dashoffset: 46.75;
+		transform: rotate(135deg);
+	}
+	100% {
+		stroke-dashoffset: 187;
+		transform: rotate(450deg);
+	}
 `;
 
 export const spinnerRotate = keyframes`
-  0% {
-			transform: rotate(0deg);
-		}
+	0% {
+		transform: rotate(0deg);
+	}
 	100% {
-			transform: rotate(270deg);
-	  }
+		transform: rotate(270deg);
+	}
 `;
+
+export const rotate = keyframes`
+	from {
+		transform: rotate(0deg);
+	}
+
+	to {
+		transform: rotate(360deg);
+	}
+`;
+
+export const pulse = {
+	one: keyframes`
+		0% {
+			transform: scale(1);
+		}
+
+		50% {
+			transform: scale(1.1);
+		}
+
+		100% {
+			transform: scale(1);
+		}
+	`,
+	two: keyframes`
+		0% {
+			transform: scale(1);
+		}
+
+		50% {
+			transform: scale(1.2);
+		}
+
+		100% {
+			transform: scale(1);
+		}
+	`,
+	three: keyframes`
+		0% {
+			transform: scale(1);
+		}
+
+		50% {
+			transform: scale(1.3);
+		}
+
+		100% {
+			transform: scale(1);
+		}
+	`,
+};
--- src/keyframes/index.ts
+++
@@ -1,2 +0,0 @@
-export {pulse} from './pulse';
-export {rotate} from './rotate';
