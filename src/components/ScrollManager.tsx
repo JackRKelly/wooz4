be647a6e1a5f331ec34ce4892b0138c0c1ef2052
JackRKelly <kcjackkelly@gmail.com>
2021-10-29 13:22:03 -0500

    abstract debounce

--- src/components/ScrollManager.tsx
+++ src/components/ScrollManager.tsx
@@ -1,18 +1,6 @@
 import {useRouter} from 'next/router';
 import {useEffect} from 'react';
-
-function debounce<Params extends any[]>(
-	func: (...args: Params) => any,
-	timeout: number,
-): (...args: Params) => void {
-	let timer: NodeJS.Timeout;
-	return (...args: Params) => {
-		clearTimeout(timer);
-		timer = setTimeout(() => {
-			func(...args);
-		}, timeout);
-	};
-}
+import {debounce} from 'util/debouce';
 
 const pathMap = new Map();
 
