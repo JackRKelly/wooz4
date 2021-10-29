148da93e05db7688a7b5e12ea4f124ce5d93ca9d
JackRKelly <kcjackkelly@gmail.com>
2021-10-29 02:37:04 -0500

    Add success sound effect

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -11,6 +11,19 @@ const Contact: NextPage = () => (
 		</Head>
 		<h2>Contact</h2>
 		<p>This page is under construction</p>
+		<form>
+			<label htmlFor="name">Name</label>
+			<input name="name" />
+			<button
+				type="submit"
+				onClick={e => {
+					e.preventDefault();
+					void new Audio('/success.mp3').play().catch(() => null);
+				}}
+			>
+				Submit
+			</button>
+		</form>
 	</ContentColumn>
 );
 
