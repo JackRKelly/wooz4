5e1ce33f25ac3ceb0951bf7933a55d2a1545b0fa
JackRKelly <kcjackkelly@gmail.com>
2021-10-31 01:12:09 -0500

    Add text color

--- src/components/Text.styled.tsx
+++ src/components/Text.styled.tsx
@@ -10,6 +10,7 @@ export const BoldUppercaseText = styled.span`
 	text-transform: uppercase;
 	font-style: italic;
 	font-weight: bold;
+	color: ${colors.black};
 `;
 
 export const H2NoMargin = styled.h2`
