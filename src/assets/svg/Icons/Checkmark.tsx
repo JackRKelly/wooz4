86b1e5411fe3b9e582047aa86c7ec2754cc45171
Jack Kelly <jakelly@amctheatres.com>
2021-11-04 16:17:47 -0500

    Add icons

---
+++ src/assets/svg/Icons/Checkmark.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SvgStroke} from './SvgBase';
+
+export const Checkmark = () => (
+	<SvgStroke id="checkbox-checkmark" viewBox="0 0 16 24">
+		<path
+			fill="none"
+			strokeMiterlimit="10"
+			strokeWidth="2"
+			d="M1 13l4 4L15 7"
+		/>
+	</SvgStroke>
+);
---
+++ src/assets/svg/Icons/Plus.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SvgStroke} from './SvgBase';
+
+export const Plus = () => (
+	<SvgStroke id="plus" viewBox="0 0 20 24">
+		<path
+			d="M10 2v20m10-10H0"
+			fill="none"
+			strokeMiterlimit="10"
+			strokeWidth="2"
+		/>
+	</SvgStroke>
+);
